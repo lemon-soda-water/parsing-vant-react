3c4d1d3adbc163506f7929116711103f42675be5
lemon-soda-water <ysunsoda@gmail.com>
2024-01-11 03:59:59 +0800

    docs: 添加文档注释

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -1,4 +1,9 @@
-// 判断 val 是否为空值
+// 判断 val 是否是被定义的值(不为null和undefined)
+/**
+ * @description 判断 val 是否是 被定义的值(不为null和undefined)
+ * @param val any
+ * @returns 返回布尔值
+ */
 export function isDef<T>(val: T): val is NonNullable<T> {
   return val !== undefined && val !== null
 }
--- src/utils/format/unit.ts
+++ src/utils/format/unit.ts
@@ -1,6 +1,11 @@
 import { isDef } from '../base'
 import { isNumeric } from '../validate/number'
 
+/**
+ * @description 传入的数字会转换成字符串。undefined 或 null 直接返回 undefined。 如果传入的字符串是 数值 类型的字符串，则返回 `${value}px`，否则直接返回
+ * @param value string | number | undefined
+ * @returns string | undefined
+ */
 export function addUnit(value?: string | number): string | undefined {
   if (!isDef(value)) {
     return undefined
@@ -11,6 +16,11 @@ export function addUnit(value?: string | number): string | undefined {
   return isNumeric(value) ? `${value}px` : value
 }
 
+/**
+ * @description 根据 originSize 的值，返回 size。如果 originSize 是 数字 的字符串或者number类型的值，那么返回的是 `${originSize}px`，否则直接是 originSize
+ * @param originSize string | number | undefined
+ * @returns 空对象({}) 或 {width：size, height: size}
+ */
 export function getSizeStyle(originSize?: string | number) {
   if (isDef(originSize)) {
     const size = addUnit(originSize)
